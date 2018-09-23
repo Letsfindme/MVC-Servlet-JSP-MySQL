e22a34ea0660bbaa8a979cdba237f68230dbb097
fadinoufal <noufal4me@gmail.com>
2018-09-23 15:59:58 +0200

    Connection modified to meet JDBC requirements

--- src/com/mvc/util/DBConnection.java
+++ src/com/mvc/util/DBConnection.java
@@ -1,34 +1,34 @@
 package com.mvc.util;
 
 
-import java.sql.Connection;
+import com.mysql.jdbc.Connection;
+
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnection {
-    public static Connection createConnection()
-    {
+    public static Connection createConnection(){
         Connection con = null;
-        String url = "jdbc:mysql://localhost:3306/customers"; //MySQL URL and followed by the database name
-        String username = "root"; //MySQL username
-        String password = "root123"; //MySQL password
+        String url = "jdbc:mysql://localhost:3306/hello"; 
+        String username = "root";
+        String password = "root";
 
-        try
-        {
-            try
-            {
-                Class.forName("com.mysql.jdbc.Driver"); //loading mysql driver
-            }
-            catch (ClassNotFoundException e)
-            {
-                e.printStackTrace();
-            }
-            con = DriverManager.getConnection(url, username, password); //attempting to connect to MySQL database
+
+        try {
+
+            Class.forName("com.mysql.jdbc.Driver"); //loading mysql driver
+            con = (Connection) DriverManager.getConnection(url, username, password); //attempting to connect to MySQL database
             System.out.println("Printing connection object "+con);
-        }
-        catch (Exception e)
-        {
+
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+
         return con;
     }
 }
